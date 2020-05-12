99c65a4345aab603f02d89d3c34c5c9cdfd03f89
LirimIslamii <61246827+LirimIslamii@users.noreply.github.com>
2020-05-12 12:13:48 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en_US" lang="en_US" dir="ltr">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+ <meta http-equiv="X-UA-Compatible" content="IE=Edge" />  
+ <link rel="shortcut icon" href="images/favicon.ico">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>" />
+
+    <title>iCloud Activation Lock Removal</title>
+	</head>
+	<body>
+	<?php
+		require('header.php');
+	?>
+			<section class="presentation">
+				<div class="introduction">
+					<div class="intro-text">
+						<h1>iCloud® Activation Lock™ Removal</h1>
+					</div>
+						<div class="intro">
+						    <p>Select your iPhone model </label></br>	
+						    <select id="iPhone" style="color:black;">
+						    	<option value="">Select iPhone</option>
+						    	<option value="SE_new">iPhone SE</option>
+						    	<option value="XSM">iPhone XS Max</option>
+						    	<option value="XS">iPhone XS</option>
+						    	<option value="8/8+">iPhone 8/8+</option>
+						    	<option value="7/7+">iPhone 7/7+</option>
+						    	<option value="">iPhone 6S/6S+</option>
+						    	<option value="6/6+">iPhone 6/6+</option>
+						    	<option value="SE">iPhone SE</option>
+						    	<option value="5S/5C/5">iPhone 5S/5C/5</option>
+						    	<option value="4S/4">iPhone 4S/4</option>
+						    </select></br>
+						    <p>Your iPhone IMEI</p></br>
+						    <input type="text" id="IMEI" name="IMEI" /></br>
+						    <button class="unlock"><i class="fa fa-apple"></i>Unlock</button>
+						</div>
+				</div>
+				
+				<div class="cover">
+					<img src="images/lock.jpg" alt="iPhone" />
+				</div>
+			</section>
+		<section class="another">
+			<div class="unlocker">
+				<img src="images/locker.svg" id="" /><br><br>
+				<p>Removes device from the previous owners iCloud account permanently.</p>
+			</div>
+			<div class="unlocker_1">
+				<img src="images/tick.svg" alt="" /><br><br>
+				<p>Enables you to activate a device where you have forgotten the password.</p>
+			</div>
+			<div class="unlocker_2">
+				<img src="images/mobile.svg" alt="" /><br><br>
+				<p>Works with all iPhone models as well as iPhone SE up to the latest iOs 13</p>
+			</div>
+		</section>
+		<div class="photo_1">
+			<img src="images/screen.png" alt="" style="width:100%;"/>
+		</div>
+		<div class="resolve">
+			<h1>We can resolve this issue</h1>
+			<h3>in under 72 hours - from $27</h3>
+			<p>Simply use our iCloud Activation Lock Removal Service </p>
+			<p>The Activation Lock is permanently removed and the device is removed from as normal.<br>
+			the previous owners iCloud account allowing you to activate and set up the<br></p>
+			<p style="margin-left:570px;">device as normal</p>
+			<button class="unlock_1"><i class="fa fa-apple" style="color:white;"></i>Unlock</button>
+			<div class="fix">
+				<img src="images/clock.svg" id="" />
+				<p><b>24 hours unlock delivery</b><br> Instant unlocking for many<br> mobile networks.</p>
+			</div>
+			<div class="fix_1">
+				<img src="images/gear.svg" id="" />
+				<p><b>Highly Skilled Tech Team</b><br> Experienced and certified in all Apple® products.</p>
+			</div>
+			<div class="fix_2">
+				<img src="images/iphonee.svg" id="" />
+				<p><b>Your iPhone is safe</b><br> You keep your device<br>throughout the entire process.</p>
+			</div>
+		</div>
+		<img src="images/icloud.jpg" alt="" width="100px" style="margin-left:48%"/>
+		<hr width="90%" style="margin-left:60px;">
+		<div class="star">
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+            <span class="fa fa-star fa-2x checked"></span>
+            <h4> Lirim Islami </h4>
+            <p>Great website, fast iCloud lock removal.. Icloud unlocked in 2 days. great and fast service. i love this website. fast email answers and clear messages. highly recommended. Good prices.</p>
+ 		</div>
+ 		<hr width="90%" style="margin-left:60px;margin-top:50px;">
+ 		<div class="star">
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+            <span class="fa fa-star fa-2x "></span>
+            <h4> Rinor Mehmeti </h4>
+            <p>I had my doubts at first but everything went smoothly . Would recommend to anybody, only took 3 days to remove the iCloud lock and allow me to access the phone as normal, and customer service kept me informed - will be using them again thank you.</p>
+ 		</div>
+ 		<hr width="90%" style="margin-left:60px;margin-top:50px;">
+ 		<div class="star">
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+			<span class="fa fa-star fa-2x checked"></span>
+            <span class="fa fa-star fa-2x "></span>
+            <h4> Uran Lajçi </h4>
+            <p>I've had 6 phones activation unlocked so far. Only had trouble with the last one but that was because of a previous jailbreak issue software problems, although the staff at AppleiPhoneUnlock were quick to help! Cannot recommend them highly enough.</p>
+ 		</div>
+		<?php
+			require('footer.php');
+		?>
+	</body>
+</html>
