fc943e6e463e72b665a9d30a6edb0476d0e3016a
LirimIslamii <61246827+LirimIslamii@users.noreply.github.com>
2020-05-12 12:20:29 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+			<div class="logo-container">
+				<i class="fa fa-apple fa-2x fa-spin" style="color:white;"></i>
+			</div>
+			<nav>
+				<ul class="nav-links">
+					<li><a class="nav-link" href="#">iCloud Unlock</a></li>
+					<li><a class="nav-link" href="#">IMEI Check</a></li>
+					<li><a class="nav-link" href="#">SIM Unlock</a></li>
+					<li><a class="nav-link" href="#">FAQ</a></li>
+					<li><a class="nav-link" href="#">Contact Us</a></li>
+				</ul>
+			</nav>
+			
+</header>
