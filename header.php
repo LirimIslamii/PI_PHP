cf3c17d166ff3b38c165d3adb916eb829f5d8700
LirimIslamii <61246827+LirimIslamii@users.noreply.github.com>
2020-05-13 17:13:30 +0200

    Update header.php

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
 					<li><a class="nav-link" href="#">iCloud Unlock</a></li>
 					<li><a class="nav-link" href="#">IMEI Check</a></li>
 					<li><a class="nav-link" href="#">SIM Unlock</a></li>
-					<li><a class="nav-link" href="#">FAQ</a></li>
-					<li><a class="nav-link" href="#">Contact Us</a></li>
+					<li><a class="nav-link" href="#">PriceList</a></li>
+					<li><a class="nav-link" href="#">Register</a></li>
 				</ul>
 			</nav>
 			
