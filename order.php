c685967184bbd2c38418adc4b5ce85ccd314f3a2
LirimIslamii <61246827+LirimIslamii@users.noreply.github.com>
2020-05-16 00:47:15 +0200

    Update order.php

--- order.php
+++ order.php
@@ -8,8 +8,8 @@
  <link rel="shortcut icon" href="images/favicon.ico">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
- <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>" />
-   <link rel="stylesheet" href="css/style_n.css?v=<?php echo time(); ?>" />
+ <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>" />
+   <link rel="stylesheet" href="style_n.css?v=<?php echo time(); ?>" />
 
     <title>Network Check</title>
 	</head>
