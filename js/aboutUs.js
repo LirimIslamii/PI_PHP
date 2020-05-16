2264bebf420ab33250f49e577c87f5ef46d95a0e
UranLajqi <58478405+UranLajqi@users.noreply.github.com>
2020-05-17 01:00:47 +0200

    Add files via upload

---
+++ js/aboutUs.js
@@ -0,0 +1,11 @@
+function UranLajqi(){
+	document.getElementById("uran").style.display = "";
+}
+function LirimIslami(){
+	document.getElementById("lirim").style.display = "";
+}
+
+function RinorMehmeti(){
+	document.getElementById("rinor").style.display = "";
+}
+
