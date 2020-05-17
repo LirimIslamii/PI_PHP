76abbfbc945284b573d92fe79672061e796e5d00
UranLajqi <58478405+UranLajqi@users.noreply.github.com>
2020-05-17 02:11:07 +0200

    Update header.php

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
 					<li><a class="nav-link" href="index.php">iCloud Unlock</a></li>
 					<li><a class="nav-link" href="sim.php">Network Check</a></li>
 					<li><a class="nav-link" href="order.php">Order Tracking</a></li>
-					<li><a class="nav-link" href="pricelist.php">PriceList</a></li>
-					<li><a class="nav-link" href="register.php">Register</a></li>
+					<li><a class="nav-link" href="policy.php">Privacy Policy</a></li>
+					<li><a class="nav-link" href="aboutUs.php">About Us</a></li>
 				</ul>
 			</nav>
 			
