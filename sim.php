540b8eeaef2bf836afb25acc3fbfb76b28452135
LirimIslamii <61246827+LirimIslamii@users.noreply.github.com>
2020-05-17 22:18:59 +0200

    Update sim.php

--- sim.php
+++ sim.php
@@ -5,14 +5,25 @@
 <head>
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
  <meta http-equiv="X-UA-Compatible" content="IE=Edge" />  
+ <script src="js/jquery.js"></script>
+  <script src="js/ajaxreq.js"></script>
  <link rel="shortcut icon" href="images/favicon.ico">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
  <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>" />
+  <link rel="stylesheet" href="style_n.css?v=<?php echo time(); ?>" />
+
 
     <title>Network Check</title>
 	</head>
 <body>
+	<style>
+		form {
+			margin-left:555px;
+			margin-top:40px;
+
+		}
+	</style>
 	<?php
 		require('header.php');
 	?>
@@ -20,15 +31,13 @@
 		  <div class="inbox">/
 		  	<h1>Network Check</h1>
 			<h4>Your IMEI</h4><br>
-			<input type="text" placeholder="Your IMEI" id="imei" name="imei" />
-			<h4>Your Email </h4>
-			<input type="email" placeholder="Your Email" id="email" name="imei" /><br>
-			<button class="netw">Unlock now</button>
+			<input type="text" placeholder="Your IMEI" id="imei" name="imei" maxlength="15" />
 		  </div>
-		  <div class="check">
-			<img src="images/antena.jpg" style="height:150px;" alt="" />
-			<p> Only 4$ </p>
+		  <button class="netw">Check now</button>
+		 <form id="result" method="POST">
+		  </form>
 		  </div>
+		  
 	</section>
 	<div class="resolve" style="height:350px;margin-top:-70px;margin-bottom:-70px;">
 			<div class="fix">
