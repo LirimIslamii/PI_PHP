b4c33739df44ee86a8f331ac6cf9a28fd7a17dbc
UranLajqi <58478405+UranLajqi@users.noreply.github.com>
2020-05-17 22:26:29 +0200

    Update policy.php

--- policy.php
+++ policy.php
@@ -37,7 +37,7 @@
 
 	function titulli($argumenti){
 		$titulli = ucwords($argumenti);
-		echo "<h1>$titulli</h1>";
+		echo "<br><br><br><h1>$titulli</h1>";
 	}
 	
 	function paragrafi($text){
@@ -54,52 +54,61 @@ function ndarja($text){
 ?>
 <section>
 
-	<section id="id1">
+	<section id="id1" style="color: white; margin-top: 30px;">
 		<?php upper("privacy police");
-		$text = "<p>This privacy policy governs the manner in which PracticeLeague Legaltech Pvt. Ltd. collects, uses, maintains and discloses information collected from users (each, a 'User') of the www.PracticeLeague.com website ('Site'). This privacy policy applies to the Site and all products and services offered by PracticeLeague Legaltech Pvt. Ltd.</p>";
+		$file = file_get_contents('File/file1.txt');
+		$text = "<p>" . $file . "</p>";
 		paragrafi($text);
 		?>
 	</section>
 
 	<section id="id2">
 		<?php titulli("Personal identification information"); 
-		$text = "<p>We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, place an order, subscribe to the newsletter, respond to a survey, and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address, mailing address, phone number. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>";
+		$file = file_get_contents('File/file2.txt');
+		$text = "<p>" . $file . "</p>";
 		ndarja($text); ?>
 	</section>
 
 	<section id="id3">
 		<?php titulli("Non-personal identification information"); 
-		$text = "<p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>";
+		$file = file_get_contents('File/file3.txt');
+		$text = "<p>" . $file . "</p>";
 		ndarja($text); ?>
 	</section>
 
 	<section id="id4">
 		<?php titulli("Web browser cookies"); 
-		$text = "<p>Our Site may use 'cookies' to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>";
+		$file = file_get_contents('File/file4.txt');
+		$text = "<p>" . $file . "</p>";
 		ndarja($text); ?>
 	</section>
 
 	<section id="id5">
 		<?php titulli("How we use collected information"); 
-		$text = "<p>We may use the email address to send User information and updates pertaining to their order. It may also be used to respond to their inquiries, questions, and/or other requests. If User decides to opt-in to our mailing list, they will receive emails that may include company news, updates, related product or service information, etc. If at any time the User would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email or User may contact us via our Site.</p>";
+		$file = file_get_contents('File/file5.txt');
+		$text = "<p>" . $file . "</p>";
 		ndarja($text); ?>
 	</section>
 
 	<section id="id6">
-		<?php titulli("How we protect your information");
-		$text = "<p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.Sensitive and private data exchange between the Site and its Users happens over a SSL secured communication channel and is encrypted and protected with digital signatures.</p>";
+		<?php $titulli = "How We Protect Your Information";
+		echo "<h1 style='margin-left:420px;'>" . $titulli . "</h1>";
+		$file = file_get_contents('File/file6.txt');
+		$text = "<p  style='margin-left:420px;'>" . $file . "</p>";
 		ndarja($text); ?>
 	</section>
 
 	<section id="id7">
 		<?php titulli("Sharing your personal information");
-		$text = "<p>We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. We may use third party service providers to help us operate our business and the Site or administer activities on our behalf, such as sending out newsletters or surveys. We may share your information with these third parties for those limited purposes provided that you have given us your permission.</p>";
+		$file = file_get_contents('File/file7.txt');
+		$text = "<p>" . $file . "</p>";
 		ndarja($text); ?>
 	</section>
 
 	<section id="id8">
 		<?php titulli("Third party websites");
-		$text = "<p>Users may find advertising or other content on our Site that link to the sites and services of our partners, suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that appear on these sites and are not responsible for the practices employed by websites linked to or from our Site. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website's own terms and policies.</p>";
+		$file = file_get_contents('File/file8.txt');
+		$text = "<p>" . $file . "</p>";
 		ndarja($text); ?>
 	</section>
 
@@ -130,4 +139,3 @@ function ndarja($text){
 
 </body>
 </html>
-
